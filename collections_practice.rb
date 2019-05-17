921ff586782fda52209b635ae54cb032e3a93f74
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 16:37:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,12 +3,4 @@ def begins_with_r(array)
 end
 
 def contain_a(array)
-  new_array = []
-  array.select do |word|
-    puts word.split
-    new_array << word.split.include?("a")
-  end
-  puts new_array
-end
-
-contain_a(["earth", "fire", "wind", "water", "heart"])
+  return array.select{|word| word.include?("a")}
