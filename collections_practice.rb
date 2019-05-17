439ae88652ae8007f0057df0ed627d08022ff3ca
Rina Kawamura <rina.kawamura@yale.edu>
2019-05-17 16:28:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,7 @@ end
 def contain_a(array)
   new_array = []
   array.select do |word|
-    new_array = word.split.include?("a")
+    new_array << word.split.include?("a")
   end
   puts new_array
 end
